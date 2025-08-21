59401fb17536e7098be814ff2446c273ee824412
anastshak <nastyashakura@gmail.com>
2025-08-21 13:20:41 +0300

    feat: add Layout and update Home page

---
+++ src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header } from './Header';
+import { Sidebar } from './Sidebar';
+
+export const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 bg-gray-50 p-6 overflow-y-auto">{children}</main>
+      </div>
+    </div>
+  );
+};
