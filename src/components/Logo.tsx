64d2bd65562b7ae293dd24608ef72222f9e08cfb
anastshak <nastyashakura@gmail.com>
2025-08-21 16:06:16 +0300

    feat: add navigation to the home page

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,16 @@
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
 
 export const Logo = () => {
+  const navigate = useNavigate();
   return (
-    <div className="flex items-center hover:cursor-default">
-      <LeftOutlined />
-      <span className="text-2xl">/</span>
-      <RightOutlined />
+    <div className="flex items-center gap-2 hover:cursor-pointer" onClick={() => navigate('/')}>
+      <div className="flex items-center">
+        <LeftOutlined />
+        <span className="text-2xl">/</span>
+        <RightOutlined />
+      </div>
+      <h1 className="text-base font-bold font-mono">codelang</h1>
     </div>
   );
 };
