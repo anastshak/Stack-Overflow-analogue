d0bc3c263d0cfc5e723e045b37112be6270bf98e
anastshak <nastyashakura@gmail.com>
2025-08-21 16:07:00 +0300

    feat: improve request

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -6,24 +6,18 @@ export const getPosts = async (
   page: number,
   limit: number = 5,
 ): Promise<{ snippets: SnippetModel[]; meta: ApiResponse['meta'] }> => {
-  try {
-    const { data } = await axios.get(`${BASE_URL}/snippets`, { params: { page, limit } });
+  const response = await axios.get(`${BASE_URL}/snippets`, { params: { page, limit } });
+  const serverData = response.data.data;
 
-    // console.log('Fetched snippets:', data.data.data);
+  const snippets: SnippetModel[] = serverData.data.map((elem: ApiSnippet) => ({
+    id: elem.id,
+    code: elem.code,
+    language: elem.language,
+    creator: elem.user.username,
+    likes: elem.marks.filter((m) => m.type === 'like').length,
+    dislikes: elem.marks.filter((m) => m.type === 'dislike').length,
+    commentsCount: elem.comments.length,
+  }));
 
-    const snippets: SnippetModel[] = data.data.data.map((elem: ApiSnippet) => ({
-      id: elem.id,
-      code: elem.code,
-      language: elem.language,
-      creator: elem.user.username,
-      likes: elem.marks.filter((m) => m.type === 'like').length,
-      dislikes: elem.marks.filter((m) => m.type === 'dislike').length,
-      commentsCount: elem.comments.length,
-    }));
-
-    return { snippets, meta: data.data.meta };
-  } catch (error) {
-    console.error('Error fetching posts:', error);
-    throw error;
-  }
+  return { snippets, meta: serverData.meta };
 };
