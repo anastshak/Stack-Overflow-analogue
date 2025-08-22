e873e6e9d2ea7f0e96a41bce671d0d44be93eeec
anastshak <nastyashakura@gmail.com>
2025-08-22 16:35:00 +0300

    feat: update Header with authStore

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,36 @@
 import { Button } from 'antd';
 import { Logo } from './Logo';
 import { useNavigate } from 'react-router-dom';
+import { useAuthStore } from '../store/authStore';
 
 export const Header = () => {
   const navigate = useNavigate();
+  const { isAuthenticated, logout } = useAuthStore();
   return (
     <header className="flex justify-between items-center bg-blue-600 px-6 py-4 text-white">
       <Logo isDisplay />
       <div className="flex gap-4 items-center">
-        <Button
-          type="primary"
-          onClick={() => {
-            navigate('/login');
-          }}
-        >
-          Login
-        </Button>
-        <Button
-          onClick={() => {
-            navigate('/signup');
-          }}
-        >
-          Sign Up
-        </Button>
+        {isAuthenticated ? (
+          <Button onClick={logout}>Logout</Button>
+        ) : (
+          <>
+            <Button
+              type="primary"
+              onClick={() => {
+                navigate('/login');
+              }}
+            >
+              Login
+            </Button>
+            <Button
+              onClick={() => {
+                navigate('/signup');
+              }}
+            >
+              Sign Up
+            </Button>
+          </>
+        )}
       </div>
     </header>
   );
