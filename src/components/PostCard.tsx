7659a2faa3b842bf77a052f3088975664238b512
anastshak <nastyashakura@gmail.com>
2025-08-23 16:55:59 +0300

    feat: implement like/dislike functions

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,20 +1,31 @@
+import React from 'react';
 import { Card } from 'antd';
-import { CodeOutlined, CommentOutlined, DislikeOutlined, LikeOutlined, UserOutlined } from '@ant-design/icons';
+import { CodeOutlined, CommentOutlined, UserOutlined } from '@ant-design/icons';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { duotoneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { SnippetModel } from '../types';
 import cn from 'classnames';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { MarksSnap } from './Marks';
 
 interface PostCardProps {
   snippet: SnippetModel;
-  className?: string;
-  onClick?: () => void;
 }
 
-export const PostCard = ({ snippet, className, onClick }: PostCardProps) => {
+export const PostCard = ({ snippet }: PostCardProps) => {
+  const navigate = useNavigate();
+
+  const location = useLocation();
+  const hoverComments = location.pathname === '/' ? 'hover:cursor-pointer' : '';
+
+  const handleCommentsClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    navigate(`/posts/${snippet.id}`);
+  };
+
   return (
     <div className="mb-4">
-      <Card className={cn('shadow-md rounded-lg', className)} onClick={onClick}>
+      <Card className={cn('shadow-md rounded-lg')}>
         <div className="flex justify-between items-center mb-2 text-gray-500">
           <span className="font-medium">
             <UserOutlined /> {snippet.creator}
@@ -30,10 +41,10 @@ export const PostCard = ({ snippet, className, onClick }: PostCardProps) => {
         </SyntaxHighlighter>
 
         <div className="flex justify-between mt-3 text-base text-gray-600">
-          <span>
-            <LikeOutlined /> {snippet.likes} | <DislikeOutlined /> {snippet.dislikes}
-          </span>
-          <span>
+          {/* marks */}
+          <MarksSnap snippet={snippet} />
+          {/* comments */}
+          <span onClick={handleCommentsClick} className={hoverComments}>
             <CommentOutlined className="mr-1" /> {snippet.commentsCount}
           </span>
         </div>
