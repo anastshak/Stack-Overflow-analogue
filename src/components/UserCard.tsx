1eeed2afc119051e7c1f353f4e76481aea6ce8e5
anastshak <nastyashakura@gmail.com>
2025-08-26 21:30:29 +0300

    style: fix styles

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -15,13 +15,13 @@ export const UserCard = ({ user }: UserCardCompactProps) => {
       className="hover:shadow-md transition-all duration-200 cursor-pointer"
       onClick={() => navigate(`/users/${user.id}`)}
     >
-      <div className="flex items-center mx-3">
-        <Avatar size={48} icon={<UserOutlined />} />
+      <div className="flex flex-col xl:flex-row items-center gap-3 mx-3">
+        <Avatar size={48} icon={<UserOutlined className="flex-shrink-0" />} />
 
-        <div className="flex-1 min-w-0">
-          <div className="flex items-center justify-around mx-2">
-            <h4 className="text-base font-medium truncate">{user.username}</h4>
-            <Tag color={'blue'} className="capitalize">
+        <div className="flex-1 min-w-0 max-w-[200px]">
+          <div className="flex flex-col gap-1 items-center xl:items-start justify-around mx-2">
+            <h4 className="text-base font-medium truncate w-full">{user.username}</h4>
+            <Tag color={'blue'} className="capitalize text-xs m-0">
               {user.role}
             </Tag>
           </div>
