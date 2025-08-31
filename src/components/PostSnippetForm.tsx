e80aa112625120bbe49d8a6848c481fe3718b900
anastshak <nastyashakura@gmail.com>
2025-08-31 20:48:51 +0300

    refactor: update the component for reuse and add separate Create snippet card

--- src/components/PostSnippetForm.tsx
+++ src/components/PostSnippetForm.tsx
@@ -1,17 +1,29 @@
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, Card, Form, Input, Select, message } from 'antd';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { Button, Form, Input, Select } from 'antd';
+import { useQuery } from '@tanstack/react-query';
 import { snippetSchema, SnippetFormData } from '../utils/validationSnippetSchema';
-import { createSnippet, getLanguages } from '../api/snippets';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { queryClient } from '../utils/queryClient';
+import { getLanguages } from '../api/snippets';
 import { Loader } from './Loader';
+import { DeleteTwoTone } from '@ant-design/icons';
 
-export const PostSnippetForm = () => {
-  const navigate = useNavigate();
+interface PostSnippetFormProps {
+  mode: 'create' | 'edit';
+  initialValues?: SnippetFormData;
+  onSubmit: (values: SnippetFormData) => void;
+  loading?: boolean;
+  deleteLoading?: boolean;
+  handleDeleteQuestion?: () => void;
+}
 
+export const PostSnippetForm = ({
+  mode,
+  initialValues,
+  onSubmit,
+  loading,
+  deleteLoading,
+  handleDeleteQuestion,
+}: PostSnippetFormProps) => {
   const { data: languages, isLoading: isLoadingLanguages } = useQuery({
     queryKey: ['languages'],
     queryFn: getLanguages,
@@ -20,88 +32,71 @@ export const PostSnippetForm = () => {
   const {
     control,
     handleSubmit,
-    reset,
     formState: { errors },
   } = useForm<SnippetFormData>({
     resolver: zodResolver(snippetSchema),
-    defaultValues: { language: '', code: '' },
+    defaultValues: initialValues || { language: '', code: '' },
   });
-
-  const mutation = useMutation({
-    mutationFn: (values: SnippetFormData) => createSnippet(values.language, values.code),
-    onSuccess: (data) => {
-      message.success('Snippet created!');
-      queryClient.invalidateQueries({ queryKey: ['snippets'] });
-      reset();
-      navigate(`/posts/${data.id}`);
-    },
-    onError: (err) => {
-      if (axios.isAxiosError(err)) {
-        message.error(err.response?.data?.message || 'Error creating snippet');
-      } else {
-        message.error('Failed to create snippet');
-      }
-    },
-  });
-
-  const onSubmit = (values: SnippetFormData) => {
-    mutation.mutate(values);
-  };
 
   return (
-    <div className="max-w-3xl mx-auto my-8">
+    <>
       {isLoadingLanguages ? (
         <Loader />
       ) : (
-        <Card className="shadow-md">
-          <Form layout="vertical" onFinish={handleSubmit(onSubmit)}>
-            {/* Language select */}
-            <Form.Item
-              label="Language of your snippet"
-              validateStatus={errors.language ? 'error' : ''}
-              help={errors.language?.message}
-            >
-              <Controller
-                name="language"
-                control={control}
-                render={({ field }) => (
-                  <Select
-                    {...field}
-                    value={field.value || undefined}
-                    onChange={(value) => field.onChange(value || '')}
-                    placeholder="Choose a programming language..."
-                    options={languages?.map((lang) => ({ value: lang, label: <span>{lang}</span> }))}
-                  />
-                )}
-              />
-            </Form.Item>
+        <Form layout="vertical" onFinish={handleSubmit(onSubmit)}>
+          {/* Language select */}
+          <Form.Item
+            label="Language of your snippet"
+            validateStatus={errors.language ? 'error' : ''}
+            help={errors.language?.message}
+          >
+            <Controller
+              name="language"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  value={field.value || undefined}
+                  onChange={(value) => field.onChange(value || '')}
+                  placeholder="Choose a programming language..."
+                  options={languages?.map((lang) => ({ value: lang, label: <span>{lang}</span> }))}
+                />
+              )}
+            />
+          </Form.Item>
 
-            {/* Code input */}
-            <Form.Item
-              label="Code of your snippet"
-              validateStatus={errors.code ? 'error' : ''}
-              help={errors.code?.message}
-            >
-              <Controller
-                name="code"
-                control={control}
-                render={({ field }) => (
-                  <Input.TextArea
-                    {...field}
-                    rows={10}
-                    placeholder="Write your code here..."
-                    style={{ fontFamily: 'monospace' }}
-                  />
-                )}
-              />
-            </Form.Item>
+          {/* Code input */}
+          <Form.Item
+            label="Code of your snippet"
+            validateStatus={errors.code ? 'error' : ''}
+            help={errors.code?.message}
+          >
+            <Controller
+              name="code"
+              control={control}
+              render={({ field }) => (
+                <Input.TextArea
+                  {...field}
+                  rows={10}
+                  placeholder="Write your code here..."
+                  style={{ fontFamily: 'monospace' }}
+                />
+              )}
+            />
+          </Form.Item>
 
-            <Button type="primary" htmlType="submit" loading={mutation.isPending} block>
-              Create snippet
+          <div className="flex gap-1">
+            <Button type="primary" htmlType="submit" loading={loading} block>
+              {mode === 'create' ? 'Create snippet' : 'Save changes'}
             </Button>
-          </Form>
-        </Card>
+            {mode === 'edit' && (
+              <Button color="danger" variant="solid" loading={deleteLoading} onClick={handleDeleteQuestion} block>
+                <DeleteTwoTone twoToneColor={'white'} /> Delete snippet
+              </Button>
+            )}
+          </div>
+        </Form>
       )}
-    </div>
+    </>
   );
 };
--- src/pages/PostSnippet.tsx
+++ src/pages/PostSnippet.tsx
@@ -1,11 +1,13 @@
+import { CreateSnippetCard } from '../components/CreateSnippetCard';
 import { Layout } from '../components/Layout';
-import { PostSnippetForm } from '../components/PostSnippetForm';
 
 export const PostSnippet = () => {
   return (
     <Layout>
-      <h1 className="text-center font-bold text-2xl">Create new snippet!</h1>
-      <PostSnippetForm />
+      <div className="mt-[5%]">
+        <h1 className="text-center font-bold text-2xl mb-4">Create new snippet!</h1>
+        <CreateSnippetCard />
+      </div>
     </Layout>
   );
 };
